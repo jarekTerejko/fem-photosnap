ec0e75e115ab883704b96e711842606ed8db2a17
jarekTerejko <j.terejko@gmail.com>
2020-09-26 08:13:48 +0200

    added js for mobile menu

---
+++ script.js
@@ -0,0 +1,22 @@
+const navTrigger = document.querySelector(".navbar__nav-trigger");
+const navTriggerInner = document.querySelector(".navbar__nav-trigger img");
+const nav = document.querySelector(".navbar__nav-wrapper");
+
+
+const openNav = () => {
+  nav.classList.add("active");
+  navTriggerInner.src = "./images/shared/mobile/close.svg";
+};
+const closeNav = () => {
+  nav.classList.remove("active");
+  navTriggerInner.src = "./images/shared/mobile/menu.svg";
+};
+
+navTrigger.addEventListener("click", (e) => {
+  if (nav.classList.contains("active")) {
+    closeNav();
+  } else {
+    openNav();
+  }
+});
+
