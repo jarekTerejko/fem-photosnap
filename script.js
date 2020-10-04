26f4c27e716eb29239987dd146bf581a862c9846
jarekTerejko <j.terejko@gmail.com>
2020-10-04 06:09:41 +0200

    added functions to eventListener

--- script.js
+++ script.js
@@ -30,24 +30,32 @@ const labelYearly = document.querySelector(".pricing-plans__label-yearly");
 const pricesMonthly = [19.0, 39.0, 99.0];
 const pricesYearly = [190.0, 390.0, 990.0];
 
-checkbox.addEventListener("click", () => {
+const toggleYearly = () => {
+  priceElements.forEach((priceEl, i) => {
+    priceEl.textContent = `$${pricesYearly[i]}.00`;
+  });
+  descriptionElements.forEach((el) => {
+    el.textContent = "per year";
+  });
+  labelMonthly.classList.remove("active");
+  labelYearly.classList.add("active");
+};
+
+const toggleMonthly = () => {
+  priceElements.forEach((priceEl, i) => {
+    priceEl.textContent = `$${pricesMonthly[i]}.00`;
+  });
+  descriptionElements.forEach((el) => {
+    el.textContent = "per month";
+  });
+  labelYearly.classList.remove("active");
+  labelMonthly.classList.add("active");
+};
+
+checkbox.addEventListener("click", (e) => {
   if (checkbox.checked) {
-    priceElements.forEach((priceEl, i) => {
-      priceEl.textContent = `$${pricesYearly[i]}.00`;
-    });
-    descriptionElements.forEach((el) => {
-      el.textContent = "per year";
-    });
-    labelMonthly.classList.remove("active");
-    labelYearly.classList.add("active");
+    toggleYearly();
   } else {
-    priceElements.forEach((priceEl, i) => {
-      priceEl.textContent = `$${pricesMonthly[i]}.00`;
-    });
-    descriptionElements.forEach((el) => {
-      el.textContent = "per month";
-    });
-    labelYearly.classList.remove("active");
-    labelMonthly.classList.add("active");
+    toggleMonthly();
   }
 });
